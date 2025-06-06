bcaa94686653eb45ac1d4dee8d721a1cb5eca25f
Artur8822 <122230600+Artur8822@users.noreply.github.com>
2025-06-06 16:22:16 +0200

    Format code with black

--- test_process.py
+++ test_process.py
@@ -11,29 +11,29 @@ from process import import_clients, is_valid_client
 #     assert not is_valid_client("Anna", "anna@example", "abc")
 #     assert not is_valid_client("Anna", "anna@example", "")
 
-# using parametrization for better test coverage and readability
-#test 1: check validation logic (individual validation scenarios)
-@pytest.mark.parametrize(
-    "name, email, age, expected", # individual validation scenarios
-    [
-        ('Anna', 'anna@example.com', '30', True), # case is valid
-        ('', 'no_name@example.com', 30, False), # empty name
-        ('Anna', '', 30, False), # empty email
-        ('Anna', 'anna@example', '-1', False), # negative age
-        ('Anna', 'anna@example', 'abc', False), # non-integer age
-        ('Anna', 'anna@example', '', False), # empty age
 
-        
-    ]
+# using parametrization for better test coverage and readability
+# test 1: check validation logic (individual validation scenarios)
+@pytest.mark.parametrize(
+    "name, email, age, expected",  # individual validation scenarios
+    [
+        ("Anna", "anna@example.com", "30", True),  # case is valid
+        ("", "no_name@example.com", 30, False),  # empty name
+        ("Anna", "", 30, False),  # empty email
+        ("Anna", "anna@example", "-1", False),  # negative age
+        ("Anna", "anna@example", "abc", False),  # non-integer age
+        ("Anna", "anna@example", "", False),  # empty age
+    ],
 )
 
 # pytest.mark.parametrize
 def test_is_valid_client(name, email, age, expected):
     assert is_valid_client(name, email, age) == expected
-    
-#test 2: simulate importing real CSV file
+
+
+# test 2: simulate importing real CSV file
 def test_import_clients(tmp_path):
-    #create temporary CSV file
+    # create temporary CSV file
     test_csv = tmp_path / "test_clients.csv"
 
     test_csv.write_text(
@@ -42,15 +42,11 @@ def test_import_clients(tmp_path):
         "Jane Smith,jane@example.com,abc\n"
     )
 
-    # Import clients from the temporary CSV file    
+    # Import clients from the temporary CSV file
     valid, invalid = import_clients(test_csv)
-    
-    assert len(valid) == 1 # We expect only one valid row
-    assert valid[0]['name'] == 'John Doe'
-    assert valid[0]['age'] == 35
-    
-    assert len(invalid) == 1 # We expect one invalid row
-    
 
+    assert len(valid) == 1  # We expect only one valid row
+    assert valid[0]["name"] == "John Doe"
+    assert valid[0]["age"] == 35
 
-    
+    assert len(invalid) == 1  # We expect one invalid row
